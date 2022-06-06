a44a64a39324241c49381a150026ce2b737a1411
omar2004712 <o.bahutair@gmail.com>
2022-06-06 13:59:00 +0400

    implemented the command nls to list fil

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const { lstat } = fs.promises;
+const { cwd } = process;
+
+fs.readdir(cwd(), async (err, files) => {
+    if(err){
+        console.log(err)
+        return;
+    }
+    const stats = files.map((file)=>{
+        return lstat(file);
+    })
+    Promise.all(stats).then((stats) => {
+        stats.forEach((stat, index)=>{
+            if(stat.isDirectory()){
+                console.log(files[index]);
+            }
+            else {
+                console.log(files[index])
+            }
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+})
